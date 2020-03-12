407548f2b02d0f9cb2fa2b9fd7bc70c5cf8872c3
Mon nom <nom@nom.com>
2020-03-12 00:27:48 -0400

    modif3

--- ConsoleApplication9/ConsoleApplication9.cpp
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -7,6 +7,8 @@ int main()
 {
     std::cout << "Hello 11111111111!\n";
     std::cout << "Hello 222222222222!\n";
+    std::cout << "Hello 333333333333!\n";
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -15,8 +17,4 @@ int main()
 // Tips for Getting Started: 
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-i
+//   3. Use the Output window to see build output and ot
